bbc542a6f4ef107a96e68d3d0fde3c983283653d
Ernie Casilla <ecasilla@icloud.com>
2013-10-21 19:37:01 -0400

    removed column string

---
+++ db/migrate/20131021233219_remove_column_string.rb
@@ -0,0 +1,5 @@
+class RemoveColumnString < ActiveRecord::Migration
+  def change
+    remove_column :users, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131021192613) do
+ActiveRecord::Schema.define(version: 20131021233219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20131021192613) do
     t.string   "name",            null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
-    t.string   "string",          null: false
     t.integer  "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
