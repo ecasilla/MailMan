bbc5f66a57a5828ba71fcf386af599b169657294
Ernie Casilla <ecasilla@icloud.com>
2013-10-21 20:48:59 -0400

    friendly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, :name, presence: true, uniqueness: true
-
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
   has_secure_password
 
 
