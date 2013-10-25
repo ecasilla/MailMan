b52711937ebc517f903d6a1fbf712b7ea6f91602
Ernie Casilla <ecasilla@icloud.com>
2013-10-25 16:14:58 -0400

    removed user mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      CampaignMailer.welcome_email(@user).deliver
       log_in(@user)
       redirect_to user_path(@user)
     else
